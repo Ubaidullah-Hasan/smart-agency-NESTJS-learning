ac1a441605a81c63228c697b5fd62b907dbdb360
Ubaidullah-Hasan <hm2964133@gmail.com>
2025-07-27 09:01:26 +0600

    product service crud

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ডিকোরেটর না থাকা প্রপার্টি ফিল্টার করে
+      forbidNonWhitelisted: true, // হোয়াইটলিস্টে নেই এমন প্রপার্টি থাকলে এরর দেবে
+      transform: true, // পেয়লোডকে স্বয়ংক্রিয়ভাবে DTO ক্লাসে কনভার্ট করবে
+      transformOptions: {
+        enableImplicitConversion: true, // ইমপ্লিসিট কনভার্শন চালু করে
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductService {
-  getAllProducts(): string {
-    return 'List of all products';
+  private products: (CreateProductDto & { id: number })[] = [];
+  // : Array<CreateProductDto & { id: number } >
+
+  createProduct(productData: CreateProductDto) {
+    const newProduct = {
+      id: this.products.length + 1,
+      ...productData,
+    };
+    this.products.push(newProduct);
+    return newProduct;
   }
+
+  getAllProducts(): (CreateProductDto & { id: number })[] {
+    return this.products;
+  }
+
+  findOneProduct(id: number) {
+    return this.products.find((p) => p.id === id);
+  }
+
+  updateProduct(id: number, data: CreateProductDto) {
+    const product = this.products.find((p) => p.id === id);
+    if (product) {
+      product.name = data.name;
+      product.price = data.price;
+      return product;
+    }
+    return null;
+  }
+
+  deleteProduct(id: number) {
+    this.products = this.products.filter((p) => p.id !== id);
+    return { message: 'Deleted' };
+  }
+
 }
